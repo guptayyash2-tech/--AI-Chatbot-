a1d6f33e726a6a792d144a0b9bc8ab9ed2943298
Yash Gupta <guptayyash2@gmail.com>
2026-02-14 21:31:05 +0530

    backend

--- Backend/Router/Userrouter.js
+++ Backend/Router/Userrouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Userregister = require('../Controller/Userlogin');
 const chatWithAI = require('../Openai/openai');
+const adminprotect = require('../Midllewear/Usermiddle');
 const userrouter = express.Router();
 
 userrouter.post("/register",Userregister);
-userrouter.post("/chatapi",chatWithAI.chatWithAI);
-userrouter.get("/chat/history",chatWithAI.chathistory)
+userrouter.post("/chatapi",adminprotect,chatWithAI.chatWithAI);
+userrouter.get("/chat/history",adminprotect,chatWithAI.chathistory)
 
 module.exports = userrouter;
