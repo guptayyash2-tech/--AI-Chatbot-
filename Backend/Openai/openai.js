cd281755d1d7b1da1956aec9582450d31e9cea87
Yash Gupta <guptayyash2@gmail.com>
2026-02-15 14:48:07 +0530

    Update openai.js

--- Backend/Openai/openai.js
+++ Backend/Openai/openai.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 // stable model
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash"
+  model: "gemini-1.5-pro"
 });
 
 const chatWithAI = async (req, res) => {
